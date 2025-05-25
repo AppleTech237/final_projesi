286c05aecffd0f7baa62679269a249a756065cbb
GAIUS JORESSE <86877090+AppleTech237@users.noreply.github.com>
2025-05-26 00:44:10 +0300

    Update app.py

--- 2.hafta/app.py
+++ 2.hafta/app.py
@@ -4,6 +4,8 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from sqlalchemy.sql import func
+import os
+
 
 
 app = Flask(__name__)
